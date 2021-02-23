770f313e34eddeab811bb8ed7dfbb846ee828337
Vit <test@epam.com>
2021-02-23 15:26:03 +0200

    Add unit tests for backend

---
+++ backend/test/index.spec.js
@@ -0,0 +1,15 @@
+  const expect = require('chai').expect;
+
+describe('set of unit tests', function () {
+    it('unit test 1', function () {
+        expect(2 + 2).to.equal(4);
+    });
+
+    it('unit test 2', function () {
+        expect(2 * 3).to.equal(6);
+    });
+
+    it('unit test 3', function () {
+        expect(2 / 2).to.equal(1);
+    });
+});
